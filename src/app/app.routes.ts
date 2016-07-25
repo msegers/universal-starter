d4281ea2b7814be92c29cbc4190d23b0ffb64c5a
Mathijs Segers <mathijs.segers@liquix.eu>
2016-07-25 19:34:57 +0200

    added firebase, need cli so pushing current working state

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,14 @@
 import { RouterConfig } from '@angular/router';
 import { Blog } from './blog';
 import { About } from "./about";
+import {BlogDetail} from "./blog/blog.detail.component";
 
 
 export const routes: RouterConfig = [
-  { path: '', redirectTo: 'home' },
-  { path: 'home', component: Blog },
+  { path: '', redirectTo: 'blog' },
+  { path: 'blog', component: Blog },
+  { path: 'blog/:id', component: BlogDetail },
   { path: 'about-mathijs-segers', component: About },
-  { path: 'my-history', component: About },
-  { path: '**', redirectTo: 'home' }
+  { path: 'skills-and-history', component: About },
+  { path: '**', redirectTo: 'blog' }
 ];
---
+++ src/app/blog/blog.detail.component.ts
@@ -0,0 +1,9 @@
+import {Component} from "@angular/core";
+@Component({
+    moduleId: __filename,
+    selector: 'blog-detail',
+    template: 'blog.detail.html'
+})
+export class BlogDetail {
+
+}
